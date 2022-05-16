9bbc40d1095e285e8ae5e55c5f77bf8f36e8d688
Ryan Yogan <ryanyogan@gmail.com>
2022-05-15 21:39:17 -0500

    Creation of note domain logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Footer } from "./components/Footer";
+import { Logo } from "./components/Logo";
+import styles from "./App.module.css";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <Logo />
+      <main className={styles.content}>
+        <h3>Piano here</h3>
+      </main>
+      <Footer />
     </div>
   );
-}
-
-export default App;
+};
